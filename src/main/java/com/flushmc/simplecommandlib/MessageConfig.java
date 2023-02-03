5ed0e96ce583658a66123029e4d8971b9f187feb
Bruno Moraes <moraes.7bruno@gmail.com>
2023-02-03 16:14:26 -0300

    fix default permission error colorize

--- src/main/java/com/flushmc/simplecommandlib/MessageConfig.java
+++ src/main/java/com/flushmc/simplecommandlib/MessageConfig.java
@@ -8,8 +8,12 @@ import net.md_5.bungee.api.ChatColor;
 @AllArgsConstructor
 public class MessageConfig {
 
-    @Getter @Setter
+    @Getter
     private String permissionError;
+
+    public void setPermissionError(String permissionError) {
+        this.permissionError = simpleTranslate(permissionError);
+    }
 
     public static String simpleTranslate(String message) {
         return ChatColor.translateAlternateColorCodes('&', message);
